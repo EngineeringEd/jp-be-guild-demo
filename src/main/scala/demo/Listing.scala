033589d1d21667e9b42f8a43eca8cc028a016876
edwardheaver <ed.heaver@justpark.com>
2023-07-08 12:57:29 +0100

    small changes for clarity

--- src/main/scala/demo/Listing.scala
+++ src/main/scala/demo/Listing.scala
@@ -7,13 +7,15 @@ case class Listing(
   title: String = "Test Listing",
   var cleansingState: CleansingStateEnum = CleansingStateEnum.New
 ) {
-  def getStatus: String = unapply(this) match {
-    case (true, false, false, CleansingStateEnum.Approved) => ListingStatusEnum.Active.value
-    case (false, true, true, CleansingStateEnum.Purged) => ListingStatusEnum.Deleted.value
-    case (false, false, true, CleansingStateEnum.Approved | CleansingStateEnum.New) => ListingStatusEnum.Hidden.value
+  def getStatus: String = {
+    return unapply(this) match {
+      case (true, false, false, CleansingStateEnum.Approved) => ListingStatusEnum.Active.value
+      case (false, true, true, CleansingStateEnum.Purged) => ListingStatusEnum.Deleted.value
+      case (false, false, true, CleansingStateEnum.Approved | CleansingStateEnum.New) => ListingStatusEnum.Hidden.value
+    }
   }
 
   def unapply(listing: Listing): (Boolean, Boolean, Boolean, CleansingStateEnum) = {
-    (listing.active, listing.isDeleted, listing.isHidden, listing.cleansingState)
+    return (listing.active, listing.isDeleted, listing.isHidden, listing.cleansingState)
   }
 }
